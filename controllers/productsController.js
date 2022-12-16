4b5e7c8b16468729feccdf531f125b635aeacaae
Cornelia Larzon <larzon.cornelia@gmail.com>
2022-12-16 15:00:00 +0100

    mongodb products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,51 +2,53 @@ let products = require('../data/simulated_database')
 const express = require('express')
 const controller = express.Router()
 
-const ProductSchema = require('../schemas/productSchema')
-
-controller.param("articleNumber", (req, res, next, articleNumber) => {
-    req.products = products.find(x => x.articleNumber == articleNumber)
-    next()
-})
-controller.param("tag", (req, res, next, tag) => {
-    req.products = products.filter(x => x.tag == tag)
-    next()
-})
-
-controller.route('/details/:articleNumber').get((req, res) => {
-    if(req.product != undefined)
-        res.status(200).json(req.product)
-    else
-        res.status(404).json()
-})
-
-controller.route('/:tag').get((req, res) => {
-    
-    if(req.products != undefined)
-        res.status(200).json(req.products)
-    else
-        res.status(404).json()
-})
-
-controller.route('/:tag/:take').get((req, res) => {
-    let list = []
-
-    for (let i = 0; i < Number(req.params.take); i++)
-        list.push(req.products[i])
-
-    res.status(200).json(list)
-    
-})
-
+const productSchema = require('../schemas/productSchema')
 
 controller.route('/').get( async (req, res) => {
     try {
-        res.status(200).json(await ProductSchema.find())
+        res.status(200).json(await productSchema.find())
     } catch {
         res.status(400).json()
     }
     
 })
+
+
+// controller.param("articleNumber", (req, res, next, articleNumber) => {
+//     req.products = productSchema.find(x => x.articleNumber == articleNumber)
+//     next()
+// })
+// controller.param("tag", (req, res, next, tag) => {
+//     req.products = productSchema.filter(x => x.tag == tag)
+//     next()
+// })
+
+controller.route('/details/:articleNumber').get(async (req, res) => {
+    const product = await productSchema.findById(req.params.articleNumber)
+    if(product)
+        res.status(200).json(product)
+    else
+        res.status(404).json()
+})
+
+controller.route('/:tag').get( async (req, res) => {
+    const products = await productSchema.find({ tag: req.params.tag})
+    if(products)
+        res.status(200).json(products)
+    else
+        res.status(404).json()
+})
+
+controller.route('/:tag/:take').get(async (req, res) => {
+    const products = await productSchema.find({ tag: req.params.tag}).limit(req.params.take)
+    if(products)
+       res.status(200).json(products)
+    else
+        res.status(400).json()
+    
+})
+
+
 
 module.exports = controller
 
